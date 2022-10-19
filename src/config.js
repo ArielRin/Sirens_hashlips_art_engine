2fd1aa99391aaad08a14fa579538178d3a9aaba4
Cheyne Hayworth <104659633+ArielRin@users.noreply.github.com>
2022-10-19 22:28:10 +1100

    build11

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 36,
+    growEditionSizeTo: 400,
     layersOrder: [
       { name: "Background" },
       { name: "Item" },
